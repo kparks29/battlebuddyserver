d9a8d28ce55cef24604f21a7c1686dc5fb0adec7
Kendrick Parks <parks.kendrick@gmail.com>
2016-09-17 04:01:20 -0700

    add bodyparser and add randome function

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import pg from 'pg'
 import Promise from 'promise'
+import bodyParser from 'body-parser'
 
 var app = express()
 
@@ -24,9 +25,18 @@ function query (sql, values = []) {
 	})
 }
 
+function getRandom () {
+	let random = Math.floor(Math.random() * (999 - 100 + 1)) + 100;
+	return query(`SELECT COUNT(*) AS count FROM users WHERE code=$1`, [random]).then((results) => {
+		console.log(results)
+	})
+}
+
+app.use(bodyParser.json())
 app.use(express.static(`${__dirname}/views`));
 
 app.get('/', (req, res) => {
+	getRandom()
 	res.status(200).sendFile(`${__dirname}/views/index.html`)
 })
 
@@ -34,6 +44,24 @@ app.get('/users/:id', (req, res) => {
 	let user;
 
 	query(`SELECT name, gender, coins, wins, loses, class FROM users WHERE code=$1 LIMIT 1;`, [req.params.id]).then((results) => {
+		console.log(results)
+		res.status(200).send(results)
+	}).catch((error) => {
+		res.status(400).send(error)
+	})
+})
+
+app.post('/users', (req, res) => {
+	if (!req.body.class) {
+		res.status(400).send('Missing Class')
+	}
+
+
+	query()
+
+	query(`INSERT INTO users (name, gender, coins, wins, loses, class) VALUES ($1, $2, $3, $4, $5, $6)`, [req.body.name || null, req.body.gender || null, 1000, 0, 0, req.body.class]).then(() => {
+		return query(`SELECT`)
+	}).then((results) => {
 		console.log(results)
 		res.status(200).send(results)
 	}).catch((error) => {
