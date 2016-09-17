1a39349175925fae5e9d2b4e69e89798a638d12a
Kendrick Parks <parks.kendrick@gmail.com>
2016-09-17 01:08:46 -0700

    testing db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,61 @@
 import express from 'express'
+import pg from 'pg'
+import Promise from 'promise'
+
+
+process.env.PORT = process.env.PORT || 5000
+process.env.DATABASE_URL = process.env.DATABASE_URL || 'postgres://ifnzrvwijvvxds:7abTa8XKGz9LgMl_aOWPSBdta7@ec2-54-225-81-90.compute-1.amazonaws.com:5432/d33ga0ododnh4n'
+
 
 var app = express()
 
-app.use(express.static(`${__dirname}/views`));
+// app.use(express.static(`${__dirname}/views`));
 
 app.get('/', (req, res) => {
-	res.render(`${__dirname}/views/index.html`)
+	query(`CREATE TABLE IF NOT EXISTS users (
+		id SERIAL,
+		uuid UUID,
+		name TEXT,
+		gender TEXT,
+		coins INT,
+		wins INT,
+		loses INT,
+		class TEXT
+	);`).then(() => {
+		return query(`INSERT INTO users (name, class) VALUES($1, $2);`, ['speedy', 'speed'])
+	}).then(() => {
+		return query(`SELECT * FROM users;`)
+	}).then((results) => {
+		console.log(results)
+		res.send(results)
+	}).catch((error) => {
+		console.log(error)
+		res.send(error)
+	})
 })
+
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server is listening on port ${process.env.PORT}`)
 })
+
+function query (sql, values = []) {
+	console.log('query')
+	return new Promise((resolve, reject) => {
+		pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+			if (err) {
+				console.log(err)
+				done()
+			}
+			client.query(sql, values, (err, result) => {
+				if (err) {
+					reject(err)
+				}
+				else {
+					resolve(result)
+				}
+				done()
+			})
+		})
+	})
+}
