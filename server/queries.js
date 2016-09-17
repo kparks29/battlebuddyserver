df9ef3e50aa80cd8f0e8e15e0b379228a01fffdb
Kendrick Parks <parks.kendrick@gmail.com>
2016-09-17 02:57:28 -0700

    just have in query file and manually put in

---
+++ server/queries.js
@@ -0,0 +1,31 @@
+queries = [
+	`CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		uuid UUID NOT NULL,
+		name TEXT,
+		gender TEXT,
+		coins INT,
+		wins INT,
+		loses INT,
+		class TEXT,
+		code INT
+	);`,
+	`CREATE TABLE IF NOT EXISTS items (
+		id SERIAL PRIMARY KEY,
+		uuid UUID NOT NULL
+	);`,
+	`CREATE TABLE IF NOT EXISTS loadouts (
+		id SERIAL PRIMARY KEY,
+		uuid UUID NOT NULL,
+		user_id INT REFERENCES users(id) NOT NULL,
+		weapon_item_id INT REFERENCES items(id) NOT NULL,
+		armor_item_id INT REFERENCES items(id) NOT NULL,
+		speed_item_id INT REFERENCES items(id) NOT NULL,
+	);`,
+	`CREATE TABLE IF NOT EXISTS items_purchased (
+		id SERIAL PRIMARY KEY,
+		uuid UUID NOT NULL,
+		item_id INT REFERENCES items(id) NOT NULL,
+		user_id INT REFERENCES users(id) NOT NULL
+	);`
+]
