9e1e3488af86572dabab96eb1d55d14e5feaa089
Kendrick Parks <parks.kendrick@gmail.com>
2016-09-17 20:46:31 -0700

    more debugging

--- server/index.js
+++ server/index.js
@@ -106,13 +106,13 @@ app.post('/users/:id/purchase', (req, res) => {
 		console.log('added items purchased')
 		return query(`SELECT cost FROM items WHERE id=$1;`, [parseInt(req.query.item)])
 	}).then((results) => {
-		console.log('got cost')
+		console.log('got cost', results, coins - results[0].cost)
 		return query(`UPDATE users SET coins=$1 WHERE id=$2;`, [userId, coins - results[0].cost])
 	}).then(() => {
 		console.log('updated users coins')
 		return query(`SELECT id, name, gender, coins, wins, loses, type, code, equiped_loadout_index FROM users WHERE id=$1 LIMIT 1;`, [userId])
 	}).then((results) => {
-		console.log('got user')
+		console.log('got user', results)
 		user = results[0]
 		return query(`SELECT weapon_item_id, armor_item_id, speed_item_id, name FROM loadouts WHERE user_id=$1;`, [userId])
 	}).then((results) => {
